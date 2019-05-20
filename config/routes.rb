f8ab468d544498cea13d336266c16b96329fadfb
Phan Van Khanh <khanhyd2@gmail.com>
2019-05-20 17:59:19 +0700

    Chapter3: Reslove some issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/about"
+    get "static_pages/contact"
 
-  root 'static_pages#home'
+    root "static_pages#home"
+  end
 end
