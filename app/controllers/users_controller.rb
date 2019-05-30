5595f9bb91b67232adce811822c34ec24819c572
Phan Van Khanh <khanhyd2@gmail.com>
2019-05-30 14:26:08 +0700

    Dang ky

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    return if current_user
+      flash[:danger] = t "error_user"
+      redirect_to root_path
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t "welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def current_user
+    @user = User.find_by id: params[:id]
+  end
 end
