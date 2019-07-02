754b7f989db5eae0a8351a4eb70d51b14dc6c58a
Phan Van Khanh <khanhyd2@gmail.com>
2019-07-02 10:26:22 +0700

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :users
     resources :microposts, only: [:create, :destroy]
     resources :relationships, only: [:create, :destroy]
+    resources :password_resets, only: [:new, :create, :edit, :update]
     resources :users do
       resources :following
       resources :followers
